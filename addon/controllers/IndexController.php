dc785d3b24b201d973a82d656f912eedbb67b30e
liweipei <778323309@qq.com>
2018-02-20 10:03:02 +0800

    firstsss

---
+++ addon/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+namespace addon\controllers;
+use eyii\controllers\Controller
+class  IndexController extends Controller{
+    public static $OK = 0;
+    public static $IllegalAesKey = -41001;
+    public static $IllegalIv = -41002;
+    public static $IllegalBuffer = -41003;
+    public static $DecodeBase64Error = -41004;
+}
